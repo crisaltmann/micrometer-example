81840fd98c3ed766cc187518a068fee8e4431522
Cristiano Altmann <cristiano_altmann@sicredi.com.br>
2019-05-15 20:07:57 -0300

    First commit. Initial project.

---
+++ src/main/java/com/crisaltmann/micrometerexample/MicrometerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.crisaltmann.micrometerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerExampleApplication.class, args);
+	}
+
+}
